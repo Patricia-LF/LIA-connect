14824fe2691b257e01e14772c9ac7e0ae73fd991
Patricia-LF <patricia_lf_86@hotmail.com>
2026-03-31 12:09:54 +0200

    Added initial loader in index.html and remove loader in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+const loader = document.getElementById("initial-loader");
+if (loader) loader.remove();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
