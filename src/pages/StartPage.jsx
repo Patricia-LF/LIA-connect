b258fe3cf6e3629092e59e2ba23ea12bcbbe52e8
Robin Andersson <danrobin.andersson@outlook.com>
2026-03-31 14:15:32 +0200

    Added styling to startpage

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,33 +1,68 @@
 import { useState } from "react";
 import styles from "./StartPage.module.css";
+import Button from "../components/Button";
 
 export default function StartPage() {
   const [role, setRole] = useState(null);
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>LIA Connect</h1>
+      <section className={styles.hero}>
+        <h1 className={styles.title}>LIA</h1>
+        <h1 className={styles.title}>Connect</h1>
+        <h3 className={styles.subtitle}>Connecting The Dotts</h3>
+        <div className={styles.details}>
+          <p className={styles.time}>13:00-17:00</p>
+          <p className={styles.location}>Lindholmspiren 7</p>
+        </div>
+      </section>
 
-      <p>13:00-17:00</p>
-      <p>Lindholmspiren 7</p>
+      <section className={styles.intro}>
+        <h2 className={styles.subtitle}>Hitta rätt personer att prata med</h2>
+        <h4 className={styles.introText}>
+          Välj dina områden, få en visuell profil och matcha med andra på
+          eventet.
+        </h4>
+      </section>
 
-      <h2 className={styles.subtitle}>Hitta rätt personer att prata med</h2>
+      <section className={styles.steps}>
+        <div className={styles["steps-container"]}>
+          <div className={styles["steps-number"]}>
+            <p>1</p>
+          </div>
+          <div className={styles["steps-text"]}>
+            <h4>Välj dina områden</h4>
+            <p>Markera det du jobbar med eller är intresserad av</p>
+          </div>
+        </div>
 
-      <ol>
-        <li>Välj dina områden</li>
-        <li>Få din profil</li>
-        <li>Matcha & prata</li>
-      </ol>
+        <div className={styles["steps-container"]}>
+          <div className={styles["steps-number"]}>
+            <p>2</p>
+          </div>
+          <div className={styles["steps-text"]}>
+            <h4>Få din profil</h4>
+            <p>Du får färger som representerar dina val</p>
+          </div>
+        </div>
 
-      <button className={styles.button} onClick={() => setRole("company")}>
-        Företag
-      </button>
+        <div className={styles["steps-container"]}>
+          <div className={styles["steps-number"]}>
+            <p>3</p>
+          </div>
+          <div className={styles["steps-text"]}>
+            <h4>Matcha och prata</h4>
+            <p>Markera det du jobbar med eller är intresserad av</p>
+          </div>
+        </div>
+      </section>
 
-      <button className={styles.button} onClick={() => setRole("student")}>
-        Student
-      </button>
+      <div className={styles.roleButtons}>
+        <Button onClick={() => setRole("company")}>Företag</Button>
+        <Button onClick={() => setRole("student")}>Student</Button>
+      </div>
 
-      {role && <p>You selected: {role}</p>}
+      {role && <p className={styles.selection}>You selected: {role}</p>}
     </div>
   );
 }
