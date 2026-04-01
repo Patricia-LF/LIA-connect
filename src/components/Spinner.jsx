508068cb4397ae5dfd4b266de7505aa8303e1a51
Patricia-LF <patricia_lf_86@hotmail.com>
2026-04-01 13:38:44 +0200

    Updated spinner with array of colors

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,10 +1,28 @@
 import styles from "./Spinner.module.css";
 
+const COLORS = [
+  "#ED8524",
+  "#E6513D",
+  "#BC3989",
+  "#672D6A",
+  "#016098",
+  "#2C8ABE",
+  "#019645",
+  "#1F5533",
+];
+
 export default function Spinner() {
   return (
     <div className={styles.loader}>
       {Array.from({ length: 8 }).map((_, i) => (
-        <div key={i} className={styles.dot} style={{ "--i": i }} />
+        <div
+          key={i}
+          className={styles.dot}
+          style={{
+            "--i": i,
+            "--dot-color": COLORS[i % COLORS.length],
+          }}
+        />
       ))}
     </div>
   );
